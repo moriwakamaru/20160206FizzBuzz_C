cb05040feda0f12a109b01e86ba6629b1b9be8f5
waka <wa_morita@route-zero.com>
2019-02-06 21:49:43 +0900

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _20190206FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("整数を入力してください");
+            int number = int.Parse(Console.ReadLine());
+
+            for(int i=1;i<=number;i++)
+            {
+                if(i%5<=0&&i%3<=0)
+                {
+                    Console.Write("FizzBuzz");
+
+                }else if(i%3<=0)
+                {
+                    Console.Write("Fizz");
+                }
+                else if (i % 5 <= 0)
+                {
+                    Console.Write("Buzz");
+                }else
+                {
+                    Console.Write(i);
+                }
+                Console.Write(",");
+            }
+                
+        }
+    }
+}
